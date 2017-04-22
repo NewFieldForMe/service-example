76c836a71ae143c17c1f99601de6585925cae448
RyoYamada <newfield1001@gmail.com>
2017-04-22 14:14:44 +0900

    add page component

--- web-front/src/app/app.module.ts
+++ web-front/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './component/main-page/main-page.component';
+import { LoginPageComponent } from './component/login-page/login-page.component';
+import { NavigationBarComponent } from './component/navigation-bar/navigation-bar.component';
+import { PostPageComponent } from './component/post-page/post-page.component';
+import { UserPageComponent } from './component/user-page/user-page.component';
+import { ArticlePageComponent } from './component/article-page/article-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    LoginPageComponent,
+    NavigationBarComponent,
+    PostPageComponent,
+    UserPageComponent,
+    ArticlePageComponent
   ],
   imports: [
     BrowserModule,
