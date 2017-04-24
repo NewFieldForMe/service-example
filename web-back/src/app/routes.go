99705106bc56e949ce7cd46c29f952eb6eddae94
RyoYamada <newfield1001@gmail.com>
2017-04-24 21:26:12 +0900

    divid router route

--- web-back/src/app/routes.go
+++ web-back/src/app/routes.go
@@ -1,53 +1,33 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gorilla/mux"
-)
+import "net/http"
 
 type Route struct {
-	Name        string
-	Method      string
-	Pattern     string
-	HandlerFunc http.HandlerFunc
+    Name        string
+    Method      string
+    Pattern     string
+    HandlerFunc http.HandlerFunc
 }
 
 type Routes []Route
 
-func NewRouter() *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
-	for _, route := range routes {
-		var handler http.Handler
-		handler = route.HandlerFunc
-		handler = Logger(handler, route.Name)
-
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(handler)
-	}
-	return router
-}
-
 var routes = Routes{
-	Route{
-		"Index",
-		"GET",
-		"/",
-		Index,
-	},
-	Route{
-		"TodoIndex",
-		"GET",
-		"/todos",
-		TodoIndex,
-	},
-	Route{
-		"TodoShow",
-		"GET",
-		"/todos/{todoId}",
-		TodoShow,
-	},
+    Route{
+        "Index",
+        "GET",
+        "/",
+        Index,
+    },
+    Route{
+        "TodoIndex",
+        "GET",
+        "/todos",
+        TodoIndex,
+    },
+    Route{
+        "TodoShow",
+        "GET",
+        "/todos/{todoId}",
+        TodoShow,
+    },
 }
