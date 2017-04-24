2cfffbdc680b45b44b8e18eaf77dd7e3391569bb
RyoYamada <newfield1001@gmail.com>
2017-04-24 21:34:18 +0900

    add headers

--- web-back/src/app/handlers.go
+++ web-back/src/app/handlers.go
@@ -19,6 +19,8 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 		model.Todo{Name: "Host meetup"},
 	}
 
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
